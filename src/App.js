08b3ec8bb6430be80db61af16035581d8648062a
Rodrigo Ribeiro <rodrigo.dev@outlook.pt>
2021-08-13 20:30:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiCheck, FiMinus, FiPlus } from "react-icons/fi";
 
+import { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0);
+  const options = ["React", "Vue", "Angular"];
+  const [option, setOption] = useState("");
+
+  console.log(option);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="box">
+        <div className="title">
+          <img
+            src="https://cdn.discordapp.com/attachments/678967152639737864/872949332519829505/download.png"
+            alt=""
+          />
+        </div>
+
+        <div className="form">
+          <div>
+            <h3>Quais Stickers?</h3>
+          </div>
+          {options.map((option, i) => (
+            <div>
+              <div key={i} onClick={() => setOption(option)} className="select">
+                <div>
+                  <FiCheck className="icon"> </FiCheck>
+                </div>
+              </div>
+              <p className="option">{option}</p>
+            </div>
+          ))}
+          <div>
+            <h3>Quantos stickers de cada?</h3>
+            <div className="boxCount">
+              <button
+                disabled={count === 0 ? true : false}
+                onClick={() => setCount(count - 1)}
+                className="count"
+              >
+                <FiMinus className="iconCount"></FiMinus>
+              </button>
+              <div>
+                <p className="countNumber iconCount">
+                  <input
+                    className="inputCount "
+                    type="number"
+                    onChange={(e) => setCount(parseInt(e.target.value))}
+                    value={count}
+                  />
+                </p>
+              </div>
+              <button onClick={() => setCount(count + 1)} className="count">
+                <FiPlus className="iconCount"></FiPlus>
+              </button>
+            </div>
+          </div>
+          <h3>Observações:</h3>
+          <textarea type="text" className="observations" />
+        </div>
+        <div className="footer">
+          <button className="enviar">Enviar</button>
+        </div>
+      </div>
+    </>
   );
 }
 
